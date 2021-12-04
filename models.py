3a9a5bb09dae9b9bb726721c37ff2b3810d21f64
Oliver <olliewatson101@gmail.com>
2021-12-04 14:32:27 +0000

    commenting out user verification temporarily

--- models.py
+++ models.py
@@ -17,41 +17,41 @@ def decrypt(data, key):
 
 
 '''
-User Model class for user to save draw data in
+User Model class for user to save data in
 '''
 
 
-class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    # User authentication information.
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
-    pin_key = db.Column(db.String(100), nullable=False)
-
-    # User information
-    firstname = db.Column(db.String(100), nullable=False)
-    lastname = db.Column(db.String(100), nullable=False)
-    phone = db.Column(db.String(100), nullable=False)
-    role = db.Column(db.String(100), nullable=False, default='user')
-
-    # User activity information
-    registered_on = db.Column(db.DateTime, nullable=False)
-    last_logged_in = db.Column(db.DateTime, nullable=True)
-    current_logged_in = db.Column(db.DateTime, nullable=True)
-
-    def __init__(self, email, firstname, lastname, phone, password, pin_key, role):
-        self.email = email
-        self.firstname = firstname
-        self.lastname = lastname
-        self.phone = phone
-        self.password = generate_password_hash(password)
-        self.pin_key = pin_key
-        self.role = role
-        self.registered_on = datetime.now()
-        self.last_logged_in = None
-        self.current_logged_in = None
+# class User(db.Model, UserMixin):
+#     __tablename__ = 'users'
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     # User authentication information.
+#     email = db.Column(db.String(100), nullable=False, unique=True)
+#     password = db.Column(db.String(100), nullable=False)
+#     pin_key = db.Column(db.String(100), nullable=False)
+#
+#     # User information
+#     firstname = db.Column(db.String(100), nullable=False)
+#     lastname = db.Column(db.String(100), nullable=False)
+#     phone = db.Column(db.String(100), nullable=False)
+#     role = db.Column(db.String(100), nullable=False, default='user')
+#
+#     # User activity information
+#     registered_on = db.Column(db.DateTime, nullable=False)
+#     last_logged_in = db.Column(db.DateTime, nullable=True)
+#     current_logged_in = db.Column(db.DateTime, nullable=True)
+#
+#     def __init__(self, email, firstname, lastname, phone, password, pin_key, role):
+#         self.email = email
+#         self.firstname = firstname
+#         self.lastname = lastname
+#         self.phone = phone
+#         self.password = generate_password_hash(password)
+#         self.pin_key = pin_key
+#         self.role = role
+#         self.registered_on = datetime.now()
+#         self.last_logged_in = None
+#         self.current_logged_in = None
 
 
 
