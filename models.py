a2b2f43682d75e27f221a96e8ac044b7eaa57b76
inesruiz <i.ruiz-blach2@newcastle.ac.uk>
2021-12-07 15:12:08 +0000

    Created the 'init_db' function in models.py, which will create a User object with the details specified when the database is initialised. Updated the 'User' class in and organised the code that contains the 'users' table set up (column assignments).

--- models.py
+++ models.py
@@ -23,30 +23,37 @@ def decrypt(data, key):
 User Model class 
 '''
 
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False, unique=True)
+
+    # User authentication information.
+    email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
-    role = db.Column(db.String(100), nullable=False)
 
-    # crypto key for user's posts
-    postkey = db.Column(db.BLOB)
-    pinkey = db.Column(db.String(100), nullable=False)
-
-    registered_on = db.Column(db.DateTime, nullable=False)
+    # User activity information
+    registered_on = db.Column(db.DateTime, nullable=True)
     last_logged_in = db.Column(db.DateTime, nullable=True)
     current_logged_in = db.Column(db.DateTime, nullable=True)
 
+    # User information
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(100), nullable=False)
+    role = db.Column(db.String(100), nullable=False, default='user')
+
     feeds = db.relationship('Post')
 
-    def __init__(self, username, password, role, pinkey):
-        self.username = username
+    def __init__(self, email, firstname, lastname, phone, password, role):
+        self.email = email
+        self.firstname = firstname
+        self.lastname = lastname
+        self.phone = phone
         self.password = generate_password_hash(password)
         self.role = role
         self.postkey = base64.urlsafe_b64encode(scrypt(password, str(get_random_bytes(32)), 32, N=2 ** 14, r=8, p=1))
-        self.pinkey = pinkey
         self.registered_on = datetime.now()
         self.last_logged_in = None
         self.current_logged_in = None
@@ -77,10 +84,16 @@ class Post(db.Model):
         self.title = decrypt(self.title, postkey)
         self.body = decrypt(self.body, postkey)
 
-    #def init_db():
-    #    db.drop_all()
-    #    db.create_all()
-    #    new_user = User(username='user1@test.com', password='mysecretpassword', role='admin',
-    #                    pinkey='BFB5S34STBLZCOB22K6PPYDCMZMH46OJ')
-    #    db.session.add(new_user)
-    #    db.session.commit()
+
+def init_db():
+    db.drop_all()
+    db.create_all()
+    admin = User(email='admin@email.com',
+                 password='Admin1!',
+                 firstname='Alice',
+                 lastname='Jones',
+                 phone='0191-123-4567',
+                 role='admin')
+
+    db.session.add(admin)
+    db.session.commit()
