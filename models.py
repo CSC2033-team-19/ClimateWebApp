21b75b908d032a774399dcd0eaf9e97c776f5710
inesruiz <i.ruiz-blach2@newcastle.ac.uk>
2021-12-09 12:48:51 +0000

    Added the length specification for the email declaration column in the 'posts' and 'challenges' table which was missing beforehand.

--- models.py
+++ models.py
@@ -69,7 +69,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, db.ForeignKey(User.email), nullable=True)
+    email = db.Column(db.String(100), db.ForeignKey(User.email), nullable=True)
     created = db.Column(db.DateTime, nullable=False)
     title = db.Column(db.Text, nullable=False, default=False)
     body = db.Column(db.Text, nullable=False, default=False)
@@ -96,7 +96,7 @@ class Challenge(db.Model):
     __tablename__ = 'challenges'
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, db.ForeignKey(User.email), nullable=True)
+    email = db.Column(db.String(100), db.ForeignKey(User.email), nullable=True)
     created = db.Column(db.DateTime, nullable=False)
     title = db.Column(db.Text, nullable=False, default=False)
     body = db.Column(db.Text, nullable=False, default=False)
