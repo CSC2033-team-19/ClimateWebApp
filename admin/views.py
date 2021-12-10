35de71728ee92efa305dae40692c813c8a505b16
inesruiz <i.ruiz-blach2@newcastle.ac.uk>
2021-12-10 22:00:09 +0000

    Created a new function which displays a list of the last 10 log entries that have been written into the log file.

--- admin/views.py
+++ admin/views.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app import requires_roles
 from models import User
-
 
 # CONFIG
 admin_blueprint = Blueprint('admin', __name__, template_folder='templates')
 
 
 # ROUTES
+# view admin homepage
 @admin_blueprint.route('/admin')
 @login_required
 @requires_roles('admin')
@@ -16,8 +16,21 @@ def admin():
     return render_template('admin.html')
 
 
+# view all registered users
 @admin_blueprint.route('/view_all_users', methods=['POST'])
 @login_required
 @requires_roles('admin')
 def view_all_users():
-    return render_template('admin.html', cur_users=User.query.all())
+    return render_template('admin.html', name=current_user.firstname, current_users=User.query.all())
+
+
+# view last 10 log entries
+@admin_blueprint.route('/logs', methods=['POST'])
+@login_required
+@requires_roles('admin')
+def logs():
+    with open("climatewebapp.log", "r") as f:
+        content = f.read().splitlines()[-10:]
+        content.reverse()
+
+    return render_template('admin.html', logs=content, name=current_user.firstname)
