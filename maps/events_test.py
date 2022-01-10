e5947a822b14ecc0443aaafddbb00ae66fe6e29e
C0024321 <C0024321@newcastle.ac.uk>
2022-01-10 12:21:22 +0000

    Change var to let in events.js

---
+++ maps/events_test.py
@@ -0,0 +1,38 @@
+import unittest
+from maps.views import *
+from models import Event
+
+class TestViews(unittest.TestCase):
+
+    def setUp(self):
+        return
+
+    def tearDown(self):
+        return
+
+    def test_map_event(self):
+        return
+
+    def test_event(self):
+        return
+
+    def test_event_id(self):
+        return
+
+    def test_handle_event(self):
+        return
+
+    def test_get_events(self):
+        return
+
+    def test_get_local_events(self):
+        return
+
+    def test_create_event(self):
+        return
+
+    def test_update_event(self):
+        return
+
+    def test_delete_event(self):
+        return
